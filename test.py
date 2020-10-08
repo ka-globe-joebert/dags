8988c3c0703a12ed764e20c9f849ff269933fcc4
Joebert Jacaba <joebert.jacaba@globe.com.ph>
2020-10-08 10:30:24 +0800

    fixing DAG

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-package main
+irflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+        'owner': 'Airflow',
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5)
+        }
+
+dag = DAG(
+        'tutorial',
+        default_args=default_args,
+        start_date=datetime(2015, 12, 1),
+        description='A simple tutorial DAG',
+        schedule_interval='@daily',
+        catchup=False)
